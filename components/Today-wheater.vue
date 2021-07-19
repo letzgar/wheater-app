0a77ab1aee535f05947f82945c7ecef73ec22213
Alejandro Martín <letzgar@gmail.com>
2021-07-19 12:10:03 +0200

    added images

---
+++ components/Today-wheater.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sidebar-container">
+    <div>
+      <button>Search for places</button>
+      <span class="material-icons"> my_location </span>
+    </div>
+    <img src="~/static/wheater-icons/Shower.png" />
+    <div>15º C</div>
+    <span>shower</span>
+    <div class="date">
+      <span>Today</span>
+      <span>.</span>
+      <span>Fri, 5 June</span>
+    </div>
+    <div class="current-localization">
+      <span class="icon"><span>Helsinki</span></span>
+    </div>
+  </div>
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ components/Upcomming-days.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="change-temperature-metric">
+      <button>ºC</button>
+      <button>ºF</button>
+    </div>
+    <div class="upcomming-days">
+      <div>Time component</div>
+    </div>
+    <div class="todays-highlights">
+      <div class="wind-status">
+        <span>Wind status</span>
+        <span>7mph</span>
+        <span>wsw</span>
+      </div>
+    </div>
+    <div class="todays-highlights">
+      <div class="wind-status">
+        <span>Wind status</span>
+        <span>7mph</span>
+        <span>wsw</span>
+      </div>
+    </div>
+    <div class="todays-highlights">
+      <div class="wind-status">
+        <span>Wind status</span>
+        <span>7mph</span>
+        <span>wsw</span>
+      </div>
+      <div class="humidity">
+        <span>Humidity</span>
+        <span>84%</span>
+        <span>progress bar</span>
+      </div>
+      <div class="Visibility">
+        <span>Visibility</span>
+        <span>6,4 miles</span>
+      </div>
+      <div class="air-presure">
+        <span>Wind status</span>
+        <span>998 mb</span>
+      </div>
+    </div>
+    <span>Created by Alejandro Martin Belchi - devChallenges.io</span>
+  </div>
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
